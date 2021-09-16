e182a6ef71a74646f0d6b3e7b1f0d89a4d7fbaa5
Chutty boy Raghav <79959547+Chuttyboy@users.noreply.github.com>
2021-09-16 13:27:15 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,109 @@
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Home extends  Component{
+    render(){
+        return(
+            <div>
+                
+// {/* <!-- //header -->
+// <!-- banner --> */}
+<div class="banner_w3lspvt" id="home">
+	<div class="csslider infinity" id="slider1">
+		<input type="radio" name="slides" checked="checked" id="slides_1"/>
+		<input type="radio" name="slides" id="slides_2"/>
+		<input type="radio" name="slides" id="slides_3"/>
+		<input type="radio" name="slides" id="slides_4"/>
+
+		<ul class="banner_slide_bg">
+			<li>
+				<div class="slider-info bg1">
+					<div class="bs-slider-overlay">
+						<div class="banner-text">
+							<div class="container">
+								<h2 class="movetxt agile-title text-capitalize">We Create and Renovate Hair Style Trends</h2>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries.</p>
+								
+								<Link to='/contact' class="btn">Get appointment</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="slider-info bg2">
+					<div class="bs-slider-overlay1">
+						<div class="banner-text">
+							<div class="container">
+								<h4 class="movetxt agile-title text-capitalize">We Help to grow your hair as well beard </h4>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries.</p>
+								<Link to='/contact' class="btn">Get appointment</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="slider-info bg3">
+					<div class="bs-slider-overlay1">
+						<div class="banner-text">
+							<div class="container">
+							<h2 class="movetxt agile-title text-capitalize">We Design and Create Hair Style Latest</h2>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries.</p>
+							<Link to='/contact' class="btn">Get appointment</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="slider-info bg4">
+					<div class="bs-slider-overlay1">
+						<div class="banner-text">
+							<div class="container">
+								<h4 class="movetxt agile-title text-capitalize">We design latest trending haircuts </h4>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries.</p>
+								<Link to='/contact' class="btn">Get appointment</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div class="navigation"> 
+			<div>
+			<label for="slides_1"></label>
+			<label for="slides_2"></label>
+			<label for="slides_3"></label>
+			<label for="slides_4"></label>
+			</div>
+		</div>
+	</div>
+</div>
+// {/* <!-- //banner -->
+//  <!-- banner bottom grids --> */}
+    <section class="content-info py-5" id="about">
+        <div class="container py-md-5">
+		<h3 class="heading text-center mb-3 mb-sm-5">About us</h3>
+
+            <div class="info-w3pvt-mid text-center px-lg-5">
+
+                <div class="title-desc text-center px-lg-5">
+					<img src="assets/images/about1.png" alt="news image" class="img-fluid" />
+                    <p class="px-lg-5">Praesent ullamcorper dui turpis.At vero eos et accusam et justo duo dolores et ea rebum.Integer sit amet mattis quam, sit amet ultricies velit. Praesent ullamcorper dui turpis.
+                        Praesent ullamcorper dui turpis.At vero eos et accusam et justo duo dolores et ea rebum.Integer sit amet mattis quam, sit amet ultricies velit. Praesent ullamcorper dui turpis.</p>
+                    
+					<Link to="/services" class="btn mt-lg-4 mt-3 read scroll" role="button">Learn More</Link>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    
+    </div>
+        )
+    }
+}
+export default Home
---
+++ src/components/Services.js
@@ -0,0 +1,63 @@
+import React,{Component} from 'react';
+ class Services extends Component{
+     render(){
+         return(
+            <div>
+            <section class="inner-page-banner" id="home">
+            </section>
+            
+            
+            
+            <div class="breadcrumb-agile">
+                <ol class="breadcrumb mb-0">
+                    <li class="breadcrumb-item">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">Services</li>
+                </ol>
+            </div>
+            
+            
+            <section class="what-we-do py-5">
+                <div class="container py-md-5">
+                <h3 class="heading text-center mb-3 mb-sm-5">Our Stylings</h3>
+                    <div class="row what-we-do-grid">
+                        <div class="col-lg-3 col-md-6 pr-0 pl-md-3 pl-0">
+                            <img src="assets/images/sp1.jpg" class="img-fluid" alt="" />
+                        </div>
+                        <div class="col-lg-3 col-md-6 bg-grid-clr">
+                            <h4 class="mt-md-0 my-2">HAIRCUTS</h4>
+                            <p class="">Sed ut perspiciatis undert sed omnis natus error .</p>
+                        </div>
+                        <div class="col-lg-3 col-md-6 pr-0 pl-md-3 pl-0 mt-lg-0 mt-4">
+                            <img src="assets/images/sp2.jpg" class="img-fluid" alt="" />
+                        </div>
+                        <div class="col-lg-3 col-md-6 bg-grid-clr mt-lg-0 mt-md-4">
+                            <h4 class="mt-md-0 my-2">SHAVES</h4>
+                            <p class="">Sed ut perspiciatis undert sed omnis natus error .</p>
+                        </div>
+                        
+                        <div class="col-lg-3 col-md-6 pr-0 pl-md-3 pl-0 mt-lg-5 mt-4">
+                            <img src="assets/images/sp3.jpg" class="img-fluid" alt="" />
+                        </div>
+                        <div class="col-lg-3 col-md-6 bg-grid-clr mt-lg-5 mt-md-4">
+                            <h4 class="mt-md-0 my-2">BEARD TRIM</h4>
+                            <p class="">Sed ut perspiciatis undert sed omnis natus error .</p>
+                        </div>
+                        <div class="col-lg-3 col-md-6 pr-0 pl-md-3 pl-0 mt-lg-5 mt-4">
+                            <img src="assets/images/sp4.jpg" class="img-fluid" alt="" />
+                        </div>
+                        <div class="col-lg-3 col-md-6 bg-grid-clr mt-lg-5 mt-md-4">
+                            <h4 class="mt-md-0 my-2">MUSTACHE TRIM</h4>
+                            <p class="">Sed ut perspiciatis undert sed omnis natus error .</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+
+                </div>
+        )
+    }
+}
+export default Services
